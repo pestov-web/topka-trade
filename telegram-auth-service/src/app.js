f79523ba89fb54296769c3ba79d4322c4271a70b
Vladimir <pestov.web@yandex.ru>
2024-12-16 19:53:34 +0300

    feat: app

---
+++ telegram-auth-service/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
