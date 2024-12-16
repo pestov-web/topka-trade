a55d85e9af603a24383ffe6496f02356116683ed
Vladimir <pestov.web@yandex.ru>
2024-12-16 19:47:01 +0300

    feat: db connection

--- telegram-auth-service/src/routes/authRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { telegramAuth } from "../controllers/authController";
-
-const router = Router();
-
-router.get("/auth/telegram", telegramAuth);
-
-export default router;
