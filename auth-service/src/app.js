480ad11e148b125243a53af82bc769509fdbd97d
Vladimir <pestov.web@yandex.ru>
2024-12-18 21:24:18 +0300

    chore(ci/cd): Dockerfiles

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -4,7 +4,7 @@ const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3050;
 
 app.use("/auth", authRoutes);
 
