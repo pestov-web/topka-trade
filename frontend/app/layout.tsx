554fb04b88cbe230144ee0a54f0f8c29763f99d0
Vladimir <pestov.web@yandex.ru>
2024-12-19 22:43:20 +0300

    chore: removing footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./ui/Header";
-import Footer from "./ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
