a9ff29b93420093df119392ae32aee69d394edeb
Vladimir Pestov <pestov.web@yandex.ru>
2025-01-16 15:44:10 +0300

    feat: charts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  sassOptions: {
-    additionalData: `@import "/app/styles/main.scss";`,
-  },
 };
 
 export default nextConfig;
